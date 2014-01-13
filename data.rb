d798b78611f8f4b3e4a795f6b4abdcc947351439
eastfoto <lishugo@163.com>
2014-01-13 17:47:49 +0800

    update data

--- data.rb
+++
@@ -1,9 +0,0 @@
-require 'open-uri'
-require 'rubygems'
-require 'nokogiri'
-
-list_of_dalian_rent = Nokogiri::HTML(open('http://rent.dl.soufun.com/'))
-
-rent_price = list_of_dalian_rent.xpath("//dd/strong")[1].content
-
-puts rent_price
